d3c70d6477725b23a4ac6444ec499b18b729d563
sanjusss <sanjusss@qq.com>
2018-02-12 09:01:43 +0800

    完成第一题

---
+++ 0001/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace _0001
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int count = nums.Length;
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = i + 1; j < count; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        int[] result = new int[2];
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ 0001Tests/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0001;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0001.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            Solution solution = new Solution();
+
+            int[] nums1 = { 2, 7, 11, 15 };
+            int target1 = 9;
+            int[] result1 = { 0, 1 };
+            CollectionAssert.AreEqual(result1, solution.TwoSum(nums1, target1));
+
+            int[] nums2 = { 3, 2, 4 };
+            int target2 = 6;
+            int[] result2 = { 1, 2 };
+            CollectionAssert.AreEqual(result2, solution.TwoSum(nums2, target2));
+        }
+    }
+}
