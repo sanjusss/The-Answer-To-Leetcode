4e2f18110fc7ca6bbb123ad5f95d2687bee8d0aa
sanjusss <sanjusss@qq.com>
2018-05-07 16:17:34 +0800

    添加第9题

---
+++ 0009/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0009
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            long src = x;
+            long des = 0;
+            long divisor = 10;
+            long bit;
+            while (src != 0)
+            {
+                src = Math.DivRem(src, divisor, out bit);
+                des = divisor * des + bit;
+            }
+
+            if (des > int.MaxValue || des < int.MinValue)
+            {
+                des = 0;
+            }
+
+            return Convert.ToInt32(des);
+        }
+
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            return x == Reverse(x);
+        }
+    }
+}
---
+++ 0009Tests/SolutionTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0009;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0009.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void IsPalindromeTest()
+        {
+            int source = 121;
+            bool target = true;
+
+            Solution solution = new Solution();
+            var result = solution.IsPalindrome(source);
+            Assert.AreEqual(target, result);
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest02()
+        {
+            int source = -121;
+            bool target = false;
+
+            Solution solution = new Solution();
+            var result = solution.IsPalindrome(source);
+            Assert.AreEqual(target, result);
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest03()
+        {
+            int source = 10;
+            bool target = false;
+
+            Solution solution = new Solution();
+            var result = solution.IsPalindrome(source);
+            Assert.AreEqual(target, result);
+        }
+    }
+}
