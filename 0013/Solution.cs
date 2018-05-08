83ce7b5ce1538f95bc3f61b1ed1584abc2591bd2
sanjusss <sanjusss@qq.com>
2018-05-08 13:43:06 +0800

    完成第13题

---
+++ 0013/Solution.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0013
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            int result = 0;
+            int count = s.Length;
+            int max = count - 1;
+            for (int i = 0; i < count; ++i)
+            {
+                char c = s[i];
+                switch (c)
+                {
+                    case 'I':
+                        if (i < max && (s[i + 1] == 'V' || s[i + 1] == 'X'))
+                        {
+                            result -= 1;
+                        }
+                        else
+                        {
+                            result += 1;
+                        }
+
+                        break;
+
+                    case 'V':
+                        result += 5;
+                        break;
+
+                    case 'X':
+                        if (i < max && (s[i + 1] == 'L' || s[i + 1] == 'C'))
+                        {
+                            result -= 10;
+                        }
+                        else
+                        {
+                            result += 10;
+                        }
+
+                        break;
+
+                    case 'L':
+                        result += 50;
+                        break;
+
+                    case 'C':
+                        if (i < max && (s[i + 1] == 'D' || s[i + 1] == 'M'))
+                        {
+                            result -= 100;
+                        }
+                        else
+                        {
+                            result += 100;
+                        }
+
+                        break;
+
+                    case 'D':
+                        result += 500;
+                        break;
+
+                    case 'M':
+                        result += 1000;
+                        break;
+
+
+                    default:
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 0013Tests/SolutionTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0013;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0013.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void RomanToIntTest()
+        {
+            string source = "III";
+            int target = 3;
+
+            Solution solution = new Solution();
+            var result = solution.RomanToInt(source);
+            Assert.AreEqual(target, result);
+        }
+
+        [TestMethod()]
+        public void RomanToIntTest2()
+        {
+            string source = "LVIII";
+            int target = 58;
+
+            Solution solution = new Solution();
+            var result = solution.RomanToInt(source);
+            Assert.AreEqual(target, result);
+        }
+
+        [TestMethod()]
+        public void RomanToIntTest3()
+        {
+            string source = "IV";
+            int target = 4;
+
+            Solution solution = new Solution();
+            var result = solution.RomanToInt(source);
+            Assert.AreEqual(target, result);
+        }
+
+        [TestMethod()]
+        public void RomanToIntTest4()
+        {
+            string source = "IX";
+            int target = 9;
+
+            Solution solution = new Solution();
+            var result = solution.RomanToInt(source);
+            Assert.AreEqual(target, result);
+        }
+
+        [TestMethod()]
+        public void RomanToIntTest5()
+        {
+            string source = "MCMXCIV";
+            int target = 1994;
+
+            Solution solution = new Solution();
+            var result = solution.RomanToInt(source);
+            Assert.AreEqual(target, result);
+        }
+    }
+}
