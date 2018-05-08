ab8eacce1488a295ef32bff4414c76ab623a593d
sanjusss <sanjusss@qq.com>
2018-05-08 13:54:18 +0800

    完成第20题

---
+++ 0020/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0020
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            try
+            {
+                Stack<char> items = new Stack<char>();
+                foreach (var i in s)
+                {
+                    switch (i)
+                    {
+                        case '(':
+                        case '[':
+                        case '{':
+                            items.Push(i);
+                            break;
+
+                        case ')':
+                            if (items.Pop() != '(')
+                            {
+                                return false;
+                            }
+
+                            break;
+
+                        case ']':
+                            if (items.Pop() != '[')
+                            {
+                                return false;
+                            }
+
+                            break;
+
+                        case '}':
+                            if (items.Pop() != '{')
+                            {
+                                return false;
+                            }
+
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+
+                return items.Count == 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 0020Tests/SolutionTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0020;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0020.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            string source = "()";
+            bool target = true;
+
+            Solution solution = new Solution();
+            var result = solution.IsValid(source);
+            Assert.AreEqual(target, result);
+        }
+
+        [TestMethod()]
+        public void IsValidTest2()
+        {
+            string source = "()";
+            bool target = true;
+
+            Solution solution = new Solution();
+            var result = solution.IsValid(source);
+            Assert.AreEqual(target, result);
+        }
+
+        [TestMethod()]
+        public void IsValidTest3()
+        {
+            string source = "()[]{}";
+            bool target = true;
+
+            Solution solution = new Solution();
+            var result = solution.IsValid(source);
+            Assert.AreEqual(target, result);
+        }
+
+        [TestMethod()]
+        public void IsValidTest4()
+        {
+            string source = "(]";
+            bool target = false;
+
+            Solution solution = new Solution();
+            var result = solution.IsValid(source);
+            Assert.AreEqual(target, result);
+        }
+
+        [TestMethod()]
+        public void IsValidTest5()
+        {
+            string source = "{[]}";
+            bool target = true;
+
+            Solution solution = new Solution();
+            var result = solution.IsValid(source);
+            Assert.AreEqual(target, result);
+        }
+    }
+}
