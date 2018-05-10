40afb3b3c5e175ed5182d08b2f2266eb3765a299
sanjusss <sanjusss@qq.com>
2018-05-10 20:01:06 +0800

    完成第26题

---
+++ 0026/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0026
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int writePos = 1;
+            int last = nums[0];
+            for (int i = 1; i < nums.Length; ++i)
+            {
+                if (nums[i] == last)
+                {
+                    continue;
+                }
+
+                last = nums[i];
+                nums[writePos] = last;
+                ++writePos;
+            }
+
+            return writePos;
+        }
+    }
+}
---
+++ 0026Tests/SolutionTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0026;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0026.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        private void AreEqual(int[] result, int count, int[] target)
+        {
+            Assert.AreEqual(true, count >= target.Length);
+            for (int i = 0; i < count; ++i)
+            {
+                Assert.AreEqual(target[i], result[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void RemoveDuplicatesTest()
+        {
+            int[] source = { 1, 1, 2 };
+            int[] target = { 1, 2 };
+
+            Solution solution = new Solution();
+            var result = solution.RemoveDuplicates(source);
+            AreEqual(source, result, target);
+        }
+
+        [TestMethod()]
+        public void RemoveDuplicatesTest2()
+        {
+            int[] source = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int[] target = { 0, 1, 2, 3, 4 };
+
+            Solution solution = new Solution();
+            var result = solution.RemoveDuplicates(source);
+            AreEqual(source, result, target);
+        }
+    }
+}
