ed278a019311e4153ecc45a41b7cf3f711ed2a80
sanjusss <sanjusss@qq.com>
2018-05-15 09:38:43 +0800

    完成第35题

---
+++ 0035/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0035
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            int index;
+            while (left < right)
+            {
+                index = left + ((right + 1 - left) >> 1);
+                if (nums[index] < target)
+                {
+                    left = index;
+                }
+                else
+                {
+                    right = index - 1;
+                }
+            }
+
+            if (nums[left] < target)
+            {
+                return left + 1;
+            }
+            else
+            {
+                return left;
+            }
+        }
+    }
+}
---
+++ 0035Tests/SolutionTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0035;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0035.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void SearchInsertTest()
+        {
+            int[] array = { 1, 3, 5, 6 };
+            int des = 5;
+            int expected = 2;
+
+            Solution solution = new Solution();
+            int actual = solution.SearchInsert(array, des);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SearchInsertTest2()
+        {
+            int[] array = { 1, 3, 5, 6 };
+            int des = 2;
+            int expected = 1;
+
+            Solution solution = new Solution();
+            int actual = solution.SearchInsert(array, des);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SearchInsertTest3()
+        {
+            int[] array = { 1, 3, 5, 6 };
+            int des = 7;
+            int expected = 4;
+
+            Solution solution = new Solution();
+            int actual = solution.SearchInsert(array, des);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SearchInsertTest4()
+        {
+            int[] array = { 1, 3, 5, 6 };
+            int des = 0;
+            int expected = 0;
+
+            Solution solution = new Solution();
+            int actual = solution.SearchInsert(array, des);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
