500e4896b6d091b6585d804e9a8c4aece46464a1
sanjusss <sanjusss@qq.com>
2018-05-16 09:22:35 +0800

    增加第38题

---
+++ 0038/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0038
+{
+    public class Solution
+    {
+        public string CountAndSay(int n)
+        {
+            if (n <= 0)
+            {
+                return string.Empty;
+            }
+            else if (n == 1)
+            {
+                return "1";
+            }
+
+            string last = CountAndSay(n - 1);
+            string result = string.Empty;
+            char current = '\0';
+            int count = 0;
+            foreach (var i in last)
+            {
+                if (i == current)
+                {
+                    ++count;
+                }
+                else
+                {
+                    if (count > 0)
+                    {
+                        result += count.ToString() + current;
+                    }
+
+                    current = i;
+                    count = 1;
+                }
+            }
+
+            if (count > 0)
+            {
+                result += count.ToString() + current;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 0038Tests/SolutionTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0038;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0038.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void CountAndSayTest()
+        {
+            int input = 1;
+            string expected = "1";
+
+            Solution solution = new Solution();
+            var actual = solution.CountAndSay(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountAndSayTest2()
+        {
+            int input = 4;
+            string expected = "1211";
+
+            Solution solution = new Solution();
+            var actual = solution.CountAndSay(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
