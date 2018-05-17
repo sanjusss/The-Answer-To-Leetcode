9fbc5f4e6cbe93958f6df6b7470d19a7842bf9cd
sanjusss <sanjusss@qq.com>
2018-05-17 09:29:50 +0800

    完成第58题

---
+++ 0058/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0058
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            string[] words = s.Split(' ');
+            for (int i = words.Length - 1; i >= 0; --i)
+            {
+                if (words[i].Length > 0)
+                {
+                    return words[i].Length;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ 0058Tests/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0058;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0058.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void LengthOfLastWordTest()
+        {
+            string input = "Hello World";
+            int expected = 5;
+
+            Solution solution = new Solution();
+            int actual = solution.LengthOfLastWord(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
