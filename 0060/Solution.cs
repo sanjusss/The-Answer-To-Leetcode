01adbfad57f193ad3af784d9747b9a3ef0f1192a
sanjusss <sanjusss@qq.com>
2018-05-18 08:41:07 +0800

    完成第60题

---
+++ 0060/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0060
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            List<int> result = new List<int>(digits);
+            result[result.Count - 1]++;
+            for (int i = result.Count - 1; i >= 0; --i)
+            {
+                if (result[i] < 10)
+                {
+                    break;
+                }
+
+                result[i] = 0;
+                if (i == 0)
+                {
+                    result.Insert(0, 1);
+                }
+                else
+                {
+                    result[i - 1]++;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ 0060Tests/SolutionTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0060;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0060.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        private void AreEqual(int[] expected, int[] actual)
+        {
+            if (expected == null)
+            {
+                if (actual != null)
+                {
+                    Assert.Fail();
+                }
+
+                return;
+            }
+
+            Assert.AreEqual(expected.Length, actual.Length);
+            int count = expected.Length;
+            for (int i = 0; i < count; ++i)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void PlusOneTest()
+        {
+            int[] input = { 1, 2, 3 };
+            int[] expected = { 1, 2, 4 };
+
+            Solution solution = new Solution();
+            var actual = solution.PlusOne(input);
+            AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PlusOneTest2()
+        {
+            int[] input = { 4, 3, 2, 1 };
+            int[] expected = { 4, 3, 2, 2 };
+
+            Solution solution = new Solution();
+            var actual = solution.PlusOne(input);
+            AreEqual(expected, actual);
+        }
+    }
+}
