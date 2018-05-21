57b39640e1c25615585fa2c15e9d84c63d3ed6fc
sanjusss <sanjusss@qq.com>
2018-05-21 09:19:13 +0800

    完成第88题

---
+++ 0088/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0088
+{
+    public class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int[] source1 = nums1.Take(m).ToArray();
+            int i = 0;
+            int j = 0;
+            while (i < m && j < n)
+            {
+                if (source1[i] <= nums2[j])
+                {
+                    nums1[i + j] = source1[i];
+                    ++i;
+                }
+                else
+                {
+                    nums1[i + j] = nums2[j];
+                    ++j;
+                }
+            }
+
+            if (i < m)
+            {
+                Array.Copy(source1, i, nums1, i + j, m - i);
+            }
+            else if (j < n)
+            {
+                Array.Copy(nums2, j, nums1, i + j, n - j);
+            }
+        }
+    }
+}
---
+++ 0088Tests/SolutionTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0088;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0088.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        private void AreEqual(int[] expected, int[] actual)
+        {
+            if (expected == null)
+            {
+                if (actual != null)
+                {
+                    Assert.Fail();
+                }
+
+                return;
+            }
+
+            Assert.AreEqual(expected.Length, actual.Length);
+            int count = expected.Length;
+            for (int i = 0; i < count; ++i)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void MergeTest()
+        {
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = { 2, 5, 6 };
+            int n = 3;
+            int[] expected = { 1, 2, 2, 3, 5, 6 };
+
+            Solution solution = new Solution();
+            solution.Merge(nums1, m, nums2, n);
+            AreEqual(expected, nums1);
+        }
+    }
+}
