a4d4b051bcfaba59ef6aa67291fbd690608475dd
sanjusss <sanjusss@qq.com>
2018-05-22 10:40:05 +0800

    完成第100题

---
+++ 0100/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0100
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+        static public TreeNode CreateNode(int?[] arr)
+        {
+            return CreateNode(arr, 0);
+        }
+
+        static private TreeNode CreateNode(int?[] arr, int index)
+        {
+            TreeNode that = arr[index] == null ? null : new TreeNode(arr[index].Value);
+            if (that != null && index * 2 + 1 < arr.Length)
+            {
+                that.left = CreateNode(arr, index * 2 + 1);
+                if (index * 2 + 2 < arr.Length)
+                {
+                    that.right = CreateNode(arr, index * 2 + 2);
+                }
+            }
+
+            return that;
+        }
+    }
+    public class Solution
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+
+            if (p == null || q == null)
+            {
+                return false;
+            }
+
+            if (p.val != q.val)
+            {
+                return false;
+            }
+
+            return IsSameTree(p.left, q.left) &&
+                IsSameTree(p.right, q.right);
+        }
+    }
+}
---
+++ 0100Tests/SolutionTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0100.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void IsSameTreeTest()
+        {
+            int?[] pArr = { 1, 2, 3 };
+            int?[] qArr = { 1, 2, 3 };
+            bool expected = true;
+
+            TreeNode p = TreeNode.CreateNode(pArr);
+            TreeNode q = TreeNode.CreateNode(qArr);
+            Solution solution = new Solution();
+            var actual = solution.IsSameTree(p, q);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsSameTreeTest2()
+        {
+            int?[] pArr = { 1, 2 };
+            int?[] qArr = { 1, null, 2 };
+            bool expected = false;
+
+            TreeNode p = TreeNode.CreateNode(pArr);
+            TreeNode q = TreeNode.CreateNode(qArr);
+            Solution solution = new Solution();
+            var actual = solution.IsSameTree(p, q);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsSameTreeTest3()
+        {
+            int?[] pArr = { 1, 2, 1 };
+            int?[] qArr = { 1, 1, 2 };
+            bool expected = false;
+
+            TreeNode p = TreeNode.CreateNode(pArr);
+            TreeNode q = TreeNode.CreateNode(qArr);
+            Solution solution = new Solution();
+            var actual = solution.IsSameTree(p, q);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsSameTreeTest4()
+        {
+            int?[] pArr = { 10,5,15 };
+            int?[] qArr = { 10, 5, null, null, 15 };
+            bool expected = false;
+
+            TreeNode p = TreeNode.CreateNode(pArr);
+            TreeNode q = TreeNode.CreateNode(qArr);
+            Solution solution = new Solution();
+            var actual = solution.IsSameTree(p, q);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
