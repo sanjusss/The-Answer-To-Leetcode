13a5e32599184ab891736008d0c89611d7baad1a
sanjusss <sanjusss@qq.com>
2018-05-24 14:17:06 +0800

    完成第104题

---
+++ 0104/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0104
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+        static public TreeNode CreateNode(int?[] arr)
+        {
+            return CreateNode(arr, 0);
+        }
+
+        static private TreeNode CreateNode(int?[] arr, int index)
+        {
+            TreeNode that = arr[index] == null ? null : new TreeNode(arr[index].Value);
+            if (that != null && index * 2 + 1 < arr.Length)
+            {
+                that.left = CreateNode(arr, index * 2 + 1);
+                if (index * 2 + 2 < arr.Length)
+                {
+                    that.right = CreateNode(arr, index * 2 + 2);
+                }
+            }
+
+            return that;
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return 1 + Math.Max(MaxDepth(root.left),
+                MaxDepth(root.right));
+        }
+    }
+}
---
+++ 0104Tests/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0104;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0104.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void MaxDepthTest()
+        {
+            int?[] arr = { 3, 9, 20, null, null, 15, 7 };
+            int expected = 3;
+
+            TreeNode tree = TreeNode.CreateNode(arr);
+            Solution solution = new Solution();
+            var actual = solution.MaxDepth(tree);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
