ddf7cc7f97e2360a543f8437155bce25d1dd33c0
sanjusss <sanjusss@qq.com>
2018-05-25 10:42:47 +0800

    完成第107题

---
+++ 0107/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0107
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+        static public TreeNode CreateNode(int?[] arr)
+        {
+            return CreateNode(arr, 0);
+        }
+
+        static private TreeNode CreateNode(int?[] arr, int index)
+        {
+            TreeNode that = arr[index] == null ? null : new TreeNode(arr[index].Value);
+            if (that != null && index * 2 + 1 < arr.Length)
+            {
+                that.left = CreateNode(arr, index * 2 + 1);
+                if (index * 2 + 2 < arr.Length)
+                {
+                    that.right = CreateNode(arr, index * 2 + 2);
+                }
+            }
+
+            return that;
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            Queue<TreeNode> nodes = new Queue<TreeNode>();
+            nodes.Enqueue(root);
+            while (nodes.Count > 0)
+            {
+                int count = nodes.Count;
+                List<int> row = new List<int>();
+                for (int i = 0; i < count; ++i)
+                {
+                    TreeNode node = nodes.Dequeue();
+                    if (node == null)
+                    {
+                        continue;
+                    }
+
+                    row.Add(node.val);
+                    nodes.Enqueue(node.left);
+                    nodes.Enqueue(node.right);
+                }
+
+                if (row.Count > 0)
+                {
+                    result.Insert(0, row);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 0107Tests/SolutionTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0107;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0107.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        private void AreEqual(IList<IList<int>> expected, IList<IList<int>> actual)
+        {
+            if (expected == null && actual == null)
+            {
+                return;
+            }
+
+            if (expected == null || actual == null)
+            {
+                Assert.Fail();
+            }
+
+            if (expected.Count != actual.Count)
+            {
+                Assert.Fail();
+            }
+
+            int count = expected.Count;
+            for (int i = 0; i < count; ++i)
+            {
+                var rowExpected = expected[i];
+                var rowActual = actual[i];
+                if (rowExpected.Count != rowActual.Count)
+                {
+                    Assert.Fail();
+                }
+
+                int itemCount = rowExpected.Count;
+                for (int j = 0; j < itemCount; ++j)
+                {
+                    Assert.AreEqual(rowExpected[j], rowActual[j]);
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void LevelOrderBottomTest()
+        {
+            int?[] input = { 3, 9, 20, null, null, 15, 7 };
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new int[] { 15, 7 });
+            expected.Add(new int[] { 9, 20 });
+            expected.Add(new int[] { 3 });
+
+            var root = TreeNode.CreateNode(input);
+            Solution solution = new Solution();
+            var actual = solution.LevelOrderBottom(root);
+            AreEqual(expected, actual);
+        }
+    }
+}
