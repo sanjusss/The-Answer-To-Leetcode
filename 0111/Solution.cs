f5018dabaed28371b7992c393dcdc8850c936030
陈玉鹏 <sanjusss@qq.com>
2018-05-26 16:17:03 +0800

    完成111题

---
+++ 0111/Solution.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0111
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+        static public TreeNode CreateNode(int?[] arr)
+        {
+            return CreateNode(arr, 0);
+        }
+
+        static private TreeNode CreateNode(int?[] arr, int index)
+        {
+            TreeNode that = arr[index] == null ? null : new TreeNode(arr[index].Value);
+            if (that != null && index * 2 + 1 < arr.Length)
+            {
+                that.left = CreateNode(arr, index * 2 + 1);
+                if (index * 2 + 2 < arr.Length)
+                {
+                    that.right = CreateNode(arr, index * 2 + 2);
+                }
+            }
+
+            return that;
+        }
+    }
+
+    public class Solution
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return MinDepthCalc(root);
+            }
+        }
+
+        private int MinDepthCalc(TreeNode root)
+        {
+            if (root == null)
+            {
+                return int.MaxValue;
+            }
+            else if (root.left == null && root.right == null)
+            {
+                return 1;
+            }
+
+            return 1 + Math.Min(MinDepthCalc(root.left),
+                MinDepthCalc(root.right));
+        }
+    }
+}
---
+++ 0111Tests/SolutionTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0111;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0111.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void MinDepthTest()
+        {
+            int?[] arr = { 3, 9, 20, null, null, 15, 7 };
+            int expected = 2;
+
+            TreeNode tree = TreeNode.CreateNode(arr);
+            Solution solution = new Solution();
+            var actual = solution.MinDepth(tree);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MinDepthTest2()
+        {
+            int?[] arr = { 1, 2 };
+            int expected = 2;
+
+            TreeNode tree = TreeNode.CreateNode(arr);
+            Solution solution = new Solution();
+            var actual = solution.MinDepth(tree);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MinDepthTest3()
+        {
+            int?[] arr = { 0 };
+            int expected = 1;
+
+            TreeNode tree = TreeNode.CreateNode(arr);
+            Solution solution = new Solution();
+            var actual = solution.MinDepth(tree);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MinDepthTest4()
+        {
+            int?[] arr = { };
+            int expected = 0;
+
+            TreeNode tree = TreeNode.CreateNode(arr);
+            Solution solution = new Solution();
+            var actual = solution.MinDepth(tree);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
