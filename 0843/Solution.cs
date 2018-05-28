1e6efc8b00fb840d4fee9b7d2ef3924515382be2
sanjusss <sanjusss@qq.com>
2018-05-28 09:48:56 +0800

    完成竞赛86的最后一题

---
+++ 0843/Solution.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0843
+{
+    public class Master
+    {
+        private readonly string secret;
+        private int maxCallTimes;
+
+        public Master(string secret, int maxCallTimes)
+        {
+            this.secret = secret;
+            this.maxCallTimes = maxCallTimes;
+        }
+
+        public int Guess(string word)
+        {
+            int same = 0;
+            int count = Math.Min(secret.Length, word.Length);
+            for (int i = 0; i < count; ++i)
+            {
+                if (secret[i] == word[i])
+                {
+                    ++same;
+                }
+            }
+
+            --maxCallTimes;
+            if (maxCallTimes < 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Console.WriteLine(string.Format("猜测为{0},相似字符数量为{1},剩余查找次数{2}。", word, same, maxCallTimes));
+            return same;
+        }
+    }
+
+    public class Solution//public声明需要在提交时去掉。
+    {
+        public void FindSecretWord(string[] wordlist, Master master)
+        {
+            List<string> words = new List<string>(wordlist);
+            Random random = new Random();
+            do
+            {
+                int index = random.Next(0, words.Count);
+                string guessWord = words[index];
+                words.RemoveAt(index);
+                int similarSize = master.Guess(guessWord);
+                if (similarSize == 6)
+                {
+                    break;
+                }
+
+                List<string> nextWords = new List<string>();
+                foreach (var i in words)
+                {
+                    int same = 0;
+                    for (int j = 0; j < 6; ++j)
+                    {
+                        if (i[j] == guessWord[j])
+                        {
+                            ++same;
+                        }
+                    }
+
+                    if (same == similarSize)
+                    {
+                        nextWords.Add(i);
+                    }
+                }
+
+                words = nextWords;
+            } while (true);
+        }
+    }
+}
---
+++ 0843Tests/SolutionTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0843;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0843.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void FindSecretWordTest()
+        {
+            string secret = "acckzz";
+            string[] wordlist = { "acckzz", "ccbazz", "eiowzz", "abcczz" };
+            int maxTry = 10;
+            Master master = new Master(secret, maxTry);
+
+            Solution solution = new Solution();
+            solution.FindSecretWord(wordlist, master);
+        }
+        
+        [TestMethod()]
+        public void FindSecretWordTest2()
+        {
+            string secret = "hbaczn";
+            string[] wordlist = { "gaxckt","trlccr","jxwhkz","ycbfps","peayuf","yiejjw","ldzccp","nqsjoa","qrjasy","pcldos","acrtag","buyeia","ubmtpj","drtclz","zqderp","snywek","caoztp","ibpghw","evtkhl","bhpfla","ymqhxk","qkvipb","tvmued","rvbass","axeasm","qolsjg","roswcb","vdjgxx","bugbyv","zipjpc","tamszl","osdifo","dvxlxm","iwmyfb","wmnwhe","hslnop","nkrfwn","puvgve","rqsqpq","jwoswl","tittgf","evqsqe","aishiv","pmwovj","sorbte","hbaczn","coifed","hrctvp","vkytbw","dizcxz","arabol","uywurk","ppywdo","resfls","tmoliy","etriev","oanvlx","wcsnzy","loufkw","onnwcy","novblw","mtxgwe","rgrdbt","ckolob","kxnflb","phonmg","egcdab","cykndr","lkzobv","ifwmwp","jqmbib","mypnvf","lnrgnj","clijwa","kiioqr","syzebr","rqsmhg","sczjmz","hsdjfp","mjcgvm","ajotcx","olgnfv","mjyjxj","wzgbmg","lpcnbj","yjjlwn","blrogv","bdplzs","oxblph","twejel","rupapy","euwrrz","apiqzu","ydcroj","ldvzgq","zailgu","xgqpsr","wxdyho","alrplq","brklfk" };
+            int maxTry = 10;
+            Master master = new Master(secret, maxTry);
+
+            Solution solution = new Solution();
+            solution.FindSecretWord(wordlist, master);
+        }
+    }
+}
