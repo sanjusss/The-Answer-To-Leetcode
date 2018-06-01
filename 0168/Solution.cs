9aef36a3101dc9fe3577b5392f2b86bbaa16fb48
sanjusss <sanjusss@qq.com>
2018-06-01 09:23:16 +0800

    增加第168题

---
+++ 0168/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0168
+{
+    public class Solution
+    {
+        public string ConvertToTitle(int n)
+        {
+            string res = string.Empty;
+            while (n > 0)
+            {
+                int remain;
+                n = Math.DivRem(n, 26, out remain);
+                if (remain == 0)
+                {
+                    remain = 26;
+                    --n;
+                }
+
+                res = Convert.ToChar('A' - 1 + remain) + res;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ 0168Tests/SolutionTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0168;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0168.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void ConvertToTitleTest()
+        {
+            int input = 1;
+            string expected = "A";
+
+            Solution solution = new Solution();
+            var actual = solution.ConvertToTitle(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ConvertToTitleTest2()
+        {
+            int input = 28;
+            string expected = "AB";
+
+            Solution solution = new Solution();
+            var actual = solution.ConvertToTitle(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ConvertToTitleTest3()
+        {
+            int input = 701;
+            string expected = "ZY";
+
+            Solution solution = new Solution();
+            var actual = solution.ConvertToTitle(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ConvertToTitleTest4()
+        {
+            int input = 52;
+            string expected = "AZ";
+
+            Solution solution = new Solution();
+            var actual = solution.ConvertToTitle(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
