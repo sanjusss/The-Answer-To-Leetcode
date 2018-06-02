c5f08ac6c1af48e97108fbafe4968138e934ab46
陈玉鹏 <sanjusss@qq.com>
2018-06-02 21:37:25 +0800

    完成第136题

---
+++ 0136/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0136
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int result = nums[0];
+            int count = nums.Length;
+            for (int i = 1; i < count; ++i)
+            {
+                result = result ^ nums[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 0136Tests/SolutionTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0136;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0136.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void SingleNumberTest()
+        {
+            int[] input = { 2, 2, 1 };
+            int expected = 1;
+
+            Solution solution = new Solution();
+            var actual = solution.SingleNumber(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SingleNumberTest2()
+        {
+            int[] input = { 4, 1, 2, 1, 2 };
+            int expected = 4;
+
+            Solution solution = new Solution();
+            var actual = solution.SingleNumber(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
