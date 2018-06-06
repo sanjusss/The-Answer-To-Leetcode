61153b39a1c291b519531584a175cffcf257eab7
陈玉鹏 <sanjusss@qq.com>
2018-06-06 20:47:06 +0800

    完成第271题。

---
+++ 0271/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0271
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> units = new HashSet<int>(nums);
+            return units.Count != nums.Length;
+        }
+    }
+}
