e7595c7b6294719492c6137db34fba4e545860b1
sanjusss <sanjusss@qq.com>
2018-06-07 08:51:35 +0800

    完成第283题。

---
+++ 0283/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0283
+{
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int length = nums.Length;
+            int write = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[write] = nums[i];
+                    write++;
+                }
+            }
+
+            for (int i = write; i < length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
---
+++ 0283Tests/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0283;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0283.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void MoveZeroesTest()
+        {
+            int[] input = { 0, 1, 0, 3, 12 };
+            int[] expected = { 1, 3, 12, 0, 0 };
+
+            Solution solution = new Solution();
+            solution.MoveZeroes(input);
+            CollectionAssert.AreEqual(expected, input);
+        }
+    }
+}
