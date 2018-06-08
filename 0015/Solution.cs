b759b9020785451389fc44e0bbd3be7d6d684e09
陈玉鹏 <sanjusss@qq.com>
2018-06-08 22:24:02 +0800

    完成第15题

---
+++ 0015/Solution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0015
+{
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            int length = nums.Length;
+            if (length < 3)
+            {
+                return result;
+            }
+
+            Array.Sort(nums);
+            HashSet<int> set = new HashSet<int>(nums);
+
+            int a = 0;
+            do
+            {
+                int c = a + 2;
+                int v;
+
+                do
+                {
+                    v = nums[c];
+                    while (c < length - 1 && v == nums[c + 1])
+                    {
+                        ++c;
+                    }
+
+                    v = 0 - nums[a] - nums[c];
+                    if (v >= nums[a] && v <= nums[c] && set.Contains(v))
+                    {
+                        bool success = false;
+                        if (nums[a] == v)
+                        {
+                            if (nums[a + 1] == v)
+                            {
+                                success = true;
+                            }
+                        }
+                        else if (nums[c] == v)
+                        {
+                            if (nums[c - 1] == v)
+                            {
+                                success = true;
+                            }
+                        }
+                        else
+                        {
+                            success = true;
+                        }
+
+                        if (success)
+                        {
+                            result.Add(new List<int>() { nums[a], v, nums[c] });
+                        }
+                    }
+
+                    ++c;
+                } while (c < length);
+
+                v = nums[a];
+                do
+                {
+                    ++a;
+                } while (a < length - 2 && v == nums[a]);
+            } while (a < length - 2 && nums[a] <= 0);
+
+            return result;
+        }
+    }
+}
---
+++ SharedCode/InputConvertor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedCode
+{
+    public class InputConvertor
+    {
+        public static IList<IList<int>> ToListList(string input)
+        {
+            input = input.Replace("],[", "#");
+            input = input.Replace("[", "");
+            input = input.Replace("]", "");
+            string[] words = input.Split('#');
+            IList<IList<int>> result = new List<IList<int>>();
+            foreach (var i in words)
+            {
+                result.Add(ToList(i));
+            }
+
+            return result;
+        }
+
+        public static IList<int> ToList(string input)
+        {
+            return new List<int>(ToArray(input));
+        }
+
+        public static int[] ToArray(string input)
+        {
+            input = input.Replace("[", "");
+            input = input.Replace("]", "");
+            string[] words = input.Split(',');
+            int[] result = new int[words.Length];
+            for (int i = 0; i < words.Length; ++i)
+            {
+                result[i] = Convert.ToInt32(words[i]);
+            }
+
+            return result;
+        }
+    }
+}
