483fa17d53a3f4af13f38ee4e8e6809e97658ac2
陈玉鹏 <sanjusss@qq.com>
2018-06-12 22:13:39 +0800

    完成第334题

---
+++ 0334/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0334
+{
+    public class Solution
+    {
+        public bool IncreasingTriplet(int[] nums)
+        {
+            int a = int.MaxValue;
+            int b = int.MaxValue;
+            foreach (var i in nums)
+            {
+                if (a >= i)
+                {
+                    a = i;
+                }
+                else if (b >= i)
+                {
+                    b = i;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
