d8d4b3e41a49147f1ea7dc3f6488da7796e5e87c
陈玉鹏 <sanjusss@qq.com>
2018-06-13 21:21:39 +0800

    完成第55题

---
+++ 0055/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0055
+{
+    public class Solution
+    {
+        public bool CanJump(int[] nums)
+        {
+            int maxStep = 0;
+            int length = nums.Length;
+            for (int i = 0; i < length - 1; ++i)
+            {
+                maxStep = Math.Max(maxStep - 1, nums[i]);
+                if (nums[i] == 0 && maxStep <= 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 0055Tests/SolutionTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0055;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0055.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        public static int[] ToArray(string input)
+        {
+            input = input.Replace("[", "");
+            input = input.Replace("]", "");
+            string[] words = input.Split(',');
+            int[] result = new int[words.Length];
+            for (int i = 0; i < words.Length; ++i)
+            {
+                result[i] = Convert.ToInt32(words[i]);
+            }
+
+            return result;
+        }
+
+        [TestMethod()]
+        public void CanJumpTest()
+        {
+            int[] nums = ToArray("[2,3,1,1,4]");
+            bool expected = true;
+
+            Solution solution = new Solution();
+            var actual = solution.CanJump(nums);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CanJumpTest2()
+        {
+            int[] nums = ToArray("[3,2,1,0,4]");
+            bool expected = false;
+
+            Solution solution = new Solution();
+            var actual = solution.CanJump(nums);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
