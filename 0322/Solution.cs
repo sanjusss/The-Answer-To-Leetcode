c2c29a4556c77c0ae030799b7c9c96250124eea5
sanjusss <sanjusss@qq.com>
2018-06-15 17:17:44 +0800

    暂时保存

---
+++ 0322/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0322
+{
+    public class Solution
+    {
+        public int CoinChange(int[] coins, int amount)
+        {
+            if (amount == 0)
+            {
+                return 0;
+            }
+
+            Array.Sort(coins);
+            int used = CoinChange(coins, amount, coins.Length);
+            return used > 0 ? used : -1;
+        }
+
+        public int CoinChange(int[] coins, int amount, int length)
+        {
+            if (length == 0)
+            {
+                return 0;
+            }
+
+            int num = coins[length - 1];
+            if (num == amount)
+            {
+                return 1;
+            }
+            else if (num > amount)
+            {
+                return CoinChange(coins, amount, length - 1);
+            }
+
+            int nextUse = CoinChange(coins, amount - num, length);
+            if (nextUse == 0)
+            {
+                return CoinChange(coins, amount, length - 1);
+            }
+            else
+            {
+                return 1 + nextUse;
+            }
+        }
+    }
+}
---
+++ 0322Tests/SolutionTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0322;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0322.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        public static int[] ToArray(string input)
+        {
+            input = input.Replace("[", "");
+            input = input.Replace("]", "");
+            string[] words = input.Split(',');
+            int[] result = new int[words.Length];
+            for (int i = 0; i < words.Length; ++i)
+            {
+                result[i] = Convert.ToInt32(words[i]);
+            }
+
+            return result;
+        }
+
+        [TestMethod()]
+        public void CoinChangeTest()
+        {
+            int[] coins = ToArray("[1, 2, 5]");
+            int amount = 11;
+            int expected = 3;
+
+            Solution solution = new Solution();
+            var actual = solution.CoinChange(coins, amount);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CoinChangeTest2()
+        {
+            int[] coins = ToArray("[2]");
+            int amount = 3;
+            int expected = -1;
+
+            Solution solution = new Solution();
+            var actual = solution.CoinChange(coins, amount);
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod()]
+        public void CoinChangeTest3()
+        {
+            int[] coins = ToArray("[186,419,83,408]");
+            int amount = 6249;
+            int expected = 20;
+
+            Solution solution = new Solution();
+            var actual = solution.CoinChange(coins, amount);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
