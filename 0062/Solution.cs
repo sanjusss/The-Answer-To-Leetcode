ed27f5194efcbb04511ddc46de56e2c5f4e6b402
sanjusss <sanjusss@qq.com>
2018-06-15 14:00:40 +0800

    完成第62题

---
+++ 0062/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0062
+{
+    public class Solution
+    {
+        public int UniquePaths(int m, int n)
+        {
+            int[,] path = new int[m, n];
+            for (int i = 0; i < m; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        path[i, j] = 1;
+                    }
+                    else
+                    {
+                        path[i, j] = path[i - 1, j] + path[i, j - 1];
+                    }
+                }
+            }
+
+            return path[m - 1, n - 1];
+        }
+    }
+}
---
+++ 0062Tests/SolutionTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0062;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0062.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void UniquePathsTest()
+        {
+            int m = 3;
+            int n = 2;
+            int expected = 3;
+
+            Solution solution = new Solution();
+            var actual = solution.UniquePaths(m, n);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void UniquePathsTest2()
+        {
+            int m = 7;
+            int n = 3;
+            int expected = 28;
+
+            Solution solution = new Solution();
+            var actual = solution.UniquePaths(m, n);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void UniquePathsTest3()
+        {
+            int m = 20;
+            int n = 20;
+            int expected = 985525432;
+
+            Solution solution = new Solution();
+            var actual = solution.UniquePaths(m, n);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
