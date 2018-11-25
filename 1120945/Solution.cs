27d59fcb98818b52a4a4ca2964a5a5a75e5436eb
sanjusss <sanjusss@qq.com>
2018-11-25 11:49:57 +0800

    完成112

---
+++ 1120945/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1120945
+{
+    public class Solution
+    {
+        public int MinIncrementForUnique(int[] A)
+        {
+            Array.Sort(A);
+            int r = 0;
+            int remains = 0;
+            int last = -1;
+            foreach (var i in A)
+            {
+                if (i == last)
+                {
+                    ++remains;
+                }
+                else
+                {
+                    int step = i - last;
+                    if (step > 1)
+                    {
+                        int u = Math.Min(step - 1, remains);
+                        remains -= u;
+                        r += (u + 1) * u / 2;
+                    }
+
+                    r += remains * step;
+                }
+
+                last = i;
+            }
+
+            r += (remains + 1) * remains / 2;
+            return r;
+        }
+    }
+}
---
+++ 1120946/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1120946
+{
+    public class Solution
+    {
+        public bool ValidateStackSequences(int[] pushed, int[] popped)
+        {
+            Stack<int> s = new Stack<int>();
+            Queue<int> p = new Queue<int>(popped);
+            foreach (var i in pushed)
+            {
+                s.Push(i);
+                while (s.Count > 0 && s.Peek() == p.Peek())
+                {
+                    s.Pop();
+                    p.Dequeue();
+                }
+            }
+
+            return s.Count == 0;
+        }
+    }
+}
