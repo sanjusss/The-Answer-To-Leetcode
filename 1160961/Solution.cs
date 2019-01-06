039e99d2ae989050595e070450d40b349f8c4b0f
sanjusss <sanjusss@qq.com>
2019-01-06 10:12:27 +0800

    116

---
+++ 1160961/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1160961
+{
+    public class Solution
+    {
+        public int RepeatedNTimes(int[] A)
+        {
+            HashSet<int> vs = new HashSet<int>();
+            foreach (var i in A)
+            {
+                if (vs.Contains(i))
+                {
+                    return i;
+                }
+                else
+                {
+                    vs.Add(i);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ 1160962/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1160962
+{
+    public class Solution
+    {
+        public int MaxWidthRamp(int[] A)
+        {
+            int res = 0;
+            int min = int.MaxValue;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] < min)
+                {
+                    min = A[i];
+                    continue;
+                }
+
+                int searchLength = i - res;
+                for (int j = 0; j < searchLength; j++)
+                {
+                    if (A[i] >= A[j])
+                    {
+                        res = i - j;
+                        break;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
